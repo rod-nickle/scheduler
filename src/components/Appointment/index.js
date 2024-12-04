262b5171790452ffe37c07169d0a5fda60cb2a30
rod-nickle <rod@bitwise-it.com>
2024-12-03 17:45:21 -0700

    Add useVisualMode custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,16 @@
 import React from "react";
-
 import "components/Appointment/styles.scss";
-
 import Header from "components/Appointment/Header";
-
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  const {history, setHistory} = useVisualMode(EMPTY);
+
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
