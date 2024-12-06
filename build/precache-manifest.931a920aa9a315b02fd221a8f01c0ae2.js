39db7f2252de4c31bb1f78069e36b6b70de23e79
rod-nickle <rod@bitwise-it.com>
2024-12-06 16:59:07 -0700

    Add build

---
+++ build/precache-manifest.931a920aa9a315b02fd221a8f01c0ae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cece6a40b1c62b596936ada7e390c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d07e3b19a12940c44895",
+    "url": "/static/css/2.17e61147.chunk.css"
+  },
+  {
+    "revision": "cfdd76d343a686c6b6d7",
+    "url": "/static/css/main.9fcafb5a.chunk.css"
+  },
+  {
+    "revision": "d07e3b19a12940c44895",
+    "url": "/static/js/2.689a5ea9.chunk.js"
+  },
+  {
+    "revision": "c91cbb924d2e6dd2f7d23f894d2489e8",
+    "url": "/static/js/2.689a5ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfdd76d343a686c6b6d7",
+    "url": "/static/js/main.31364dac.chunk.js"
+  },
+  {
+    "revision": "243b8f0ab90e65c7feeb",
+    "url": "/static/js/runtime-main.89e9d1db.js"
+  }
+]);
